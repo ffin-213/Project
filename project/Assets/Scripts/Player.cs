f58fa590ba5e2e0158b948a63cf9965bc3146937
ffin <ydkm2298@gmail.com>
2026-03-10 11:57:36 -0500

    scene set up and player movement done

---
+++ project/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 0.2f;
+    public float mouseSensitivity = 50f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
