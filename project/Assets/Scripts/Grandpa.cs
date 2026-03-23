acd74622133746c4fcbb8832fa1eb852b1148754
yixuan <ydkm2298@gmail.com>
2026-03-23 11:48:58 -0500

    grandpa movement started

--- project/Assets/Scripts/Grandpa.cs
+++ project/Assets/Scripts/Grandpa.cs
@@ -1,7 +1,13 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Grandpa : MonoBehaviour
 {
+    [SerializeField] List<GameObject> wayPoints = new List<GameObject>();
+    [SerializeField] int currentWayPoint = 0;
+    [SerializeField] float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,13 @@ public class Grandpa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 goal = wayPoints[currentWayPoint].transform.position;
+        Vector2 newPos = Vector2.MoveTowards(transform.position, wayPoints[currentWayPoint].transform.position, speed * Time.deltaTime);
+        transform.position = newPos;
+
+        if (Mathf.Abs(transform.position.x - goal.x) < .1 && Mathf.Abs(transform.position.y - goal.y) < .1)
+        {
+            currentWayPoint = (currentWayPoint + 1) % wayPoints.Count;
+        }
     }
 }
