a5b2f10dc7c9fdd892f836ea4baee33108d8d623
An07sk <anoushka010709@gmail.com>
2026-03-31 11:49:39 -0500

    3/31/26

---
+++ project/Assets/Scripts/Sound.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+    public string name;
+    public AudioClip clip;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
