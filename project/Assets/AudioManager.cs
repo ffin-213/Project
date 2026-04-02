b33346762480e657221309d0de47431bafdf446c
An07sk <anoushka010709@gmail.com>
2026-04-02 11:48:41 -0500

    4/2/26
    Finished audio and menu screen!! ;D - A

--- project/Assets/AudioManager.cs
+++ project/Assets/AudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -12,5 +13,10 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static implicit operator AudioManager(ManagerAudio v)
+    {
+        throw new NotImplementedException();
     }
 }
--- project/Assets/Scripts/ManagerAudio.cs
+++ project/Assets/Scripts/ManagerAudio.cs
@@ -1,10 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ManagerAudio : MonoBehaviour
 {
-    public AudioClip[] musicSounds, sfxSounds;
-    public AudioSource musicSource, sfxSource;
+    public static ManagerAudio Instance;
 
+    public AudioClip[] musicSounds, sfxSounds, masterSounds;
+    public AudioSource musicSource, sfxSource, masterSource;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+
+    public void PlayMusic(string name)
+    {
+        AudioClip s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Music Sound Not Found: " + name);
+        }
+        else
+        {
+            musicSource.clip = s;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        AudioClip s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("SFX Sound Not Found: " + name);
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s);
+        }
+    }
+
+    public void PlayMaster(string name)
+    {
+        AudioClip s = Array.Find(masterSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Master Sound Not Found: " + name);
+        }
+        else
+        {
+            masterSource.PlayOneShot(s);
+        }
+    }
+    public void ToggleMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+    }
+    public void ToggleSFX()
+    {
+        sfxSource.mute = !sfxSource.mute;
+    }
+    public void ToggleMaster()
+    {
+        masterSource.mute = !masterSource.mute;
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    public void SFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+    public void MasterVolume(float volume)
+    {
+        masterSource.volume = volume;
+    }
 }
