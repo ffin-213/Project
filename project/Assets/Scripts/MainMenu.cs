2545f67507f16b93be34cb37c1043305ed70f9a5
yixuan <ydkm2298@gmail.com>
2026-04-23 11:39:30 -0500

    changed a key

--- project/Assets/Scripts/MainMenu.cs
+++ project/Assets/Scripts/MainMenu.cs
@@ -15,7 +15,7 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             SceneManager.LoadScene(1);
         }
